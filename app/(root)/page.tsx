81006f1bfb9a8e4919d2589d28de01af6acdf0c9
mdkulkarni2005 <147185935+mdkulkarni2005@users.noreply.github.com>
2024-06-23 18:40:50 +0530

    Fix typo in TotalBalance BoxProps interface name

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,37 @@
+import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar'
+import TotalBalancebox from '@/components/TotalBalancebox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = {firstName: 'Manas', lastName: 'Kulkarni', email: 'contact@mdkulkarni'}
+
   return (
-    <div>Home</div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Gust'}
+            subtext="Access and manger your account and transactions efficiently."
+          />
+          <TotalBalancebox 
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+
+        RECENT TRANSACTIONS
+      </div>
+
+      <RightSidebar 
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.50 }, { currentBalance: 500.50 }]}
+      />
+    </section>
   )
 }
 
